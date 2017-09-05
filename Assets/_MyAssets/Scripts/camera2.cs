f3aea4b4887de7c51bc1cef3af24ac21dc7d1f9d
FernandoDantas <fernandoalbino755@gmail.com>
2017-09-05 16:05:46 -0400

    add player and bake in environment

---
+++ Assets/_MyAssets/Scripts/camera2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera2 : MonoBehaviour {
+
+    public GameObject target;
+    Vector3 offset; //deslocamento. Offset serve para armazenar o deslocamento entre a câmera e o jogador. Usado para manter a distância relativa ao jogador
+
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - target.transform.position;    //inserindo o valor inicial ao offset. 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.transform.position + offset;
+        transform.position = desiredPosition;
+    }
+}
