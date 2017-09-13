67de201c7dc8d49095bee98ef666d861ffe35217
Paulo Matos <paulo.matos@fpf.br>
2017-09-12 23:23:06 -0400

    add force when jumping

--- Assets/_MyAssets/Scripts/MovementController.cs
+++ Assets/_MyAssets/Scripts/MovementController.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour {
 
-	private Animator animator;
+    public float speedJump;
+    private Rigidbody rb;
+    private Animator animator;
 
-	// Use this for initialization
-	void Start () {
-		animator = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown("space"))
-            animator.SetTrigger("Jump");
-	}
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody> ();
+        animator = GetComponent<Animator>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        Vector3 movement = new Vector3 (0.0f, speedJump, 0.0f);
+
+        if (Input.GetKeyDown ("space")) {
+            animator.SetTrigger ("Jump");
+            rb.AddForce (movement);
+        }
+    }
 }
+
