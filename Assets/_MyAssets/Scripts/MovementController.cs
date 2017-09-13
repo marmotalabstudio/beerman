8bdec26de74ef86eaa308aeadc5349ee76bafbc7
Paulo Matos <paulo.matos@fpf.br>
2017-09-12 22:58:31 -0400

    jump pressing space

--- Assets/_MyAssets/Scripts/MovementController.cs
+++ Assets/_MyAssets/Scripts/MovementController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour {
 
+	private Animator animator;
+
 	// Use this for initialization
 	void Start () {
-		
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown("space"))
+            animator.SetTrigger("Jump");
 	}
 }
