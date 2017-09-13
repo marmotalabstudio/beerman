6b1d1f901ba84456de02f6fe2c397a7da8036310
Paulo Matos <paulo.matos@fpf.br>
2017-09-12 23:49:48 -0400

    player running pressing W

--- Assets/_MyAssets/Scripts/MovementController.cs
+++ Assets/_MyAssets/Scripts/MovementController.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class MovementController : MonoBehaviour {
 
     public float speedJump;
+	public float chargeSpeed;
     private Rigidbody rb;
     private Animator animator;
+	private float chargeLevel;
+	private float chargeMaxLimit = 1f;
+	private float chargeMinLimit = 0f;
 
     // Use this for initialization
     void Start () {
@@ -18,10 +22,18 @@ public class MovementController : MonoBehaviour {
     void Update () {
         Vector3 movement = new Vector3 (0.0f, speedJump, 0.0f);
 
-        if (Input.GetKeyDown ("space")) {
+        if (Input.GetKeyDown ("space") && rb.velocity.y == 0) {
             animator.SetTrigger ("Jump");
             rb.AddForce (movement);
         }
+
+		if (Input.GetKey(KeyCode.W)  && chargeLevel < chargeMaxLimit) {
+			chargeLevel += Time.deltaTime * chargeSpeed;
+		}
+		else if(chargeLevel > chargeMinLimit){
+			chargeLevel -= Time.deltaTime * chargeSpeed;
+		}
+		animator.SetFloat("movement", chargeLevel);
     }
 }
 
