4ad8b390b7f6f873ce886f64bedd467880e147f0
Jeferson Barros <im.jbalves@gmail.com>
2017-09-13 22:40:27 -0400

    created script PLayerController

---
+++ Assets/_MyAssets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rb.velocity.y < -10) {
+			Debug.Log ("morre");
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
